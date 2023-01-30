f7fb962b547c5c9ef7a7a375cb548beddbdcb5ce
praghadheeshvar <64944213+praghadheeshvar@users.noreply.github.com>
2023-01-30 19:40:33 +0530

    Add files via upload

---
+++ Github/Task2/TVPackage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class TVPackage
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Package A comtains 1)South special 2)Children's Club 3)Movies");
+            Console.WriteLine("Package B contains 1)News 2)Movies 3)Regional-2");
+            Console.WriteLine("Package C contains Discovery,National,History");
+            Console.WriteLine("Enter Choice:");
+            string ch=Console.ReadLine();
+            switch (ch)
+            {
+                case "A":
+                    Console.WriteLine("Rate:250");
+                    break;
+                case "B":
+                    Console.WriteLine("Rate:450");
+                    break;
+                case "C":
+                    Console.WriteLine("Rate:350");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+}
---
+++ Github/Task5/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class PersonDetail
+    {
+        public string Name;
+        public int Age;
+
+        public PersonDetail()
+        {
+            Name = "Praggi";
+            Age = 21;
+        }
+        public PersonDetail(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+    internal class Person
+    {
+        public static void Main(string[] args)
+        {
+            PersonDetail pobj1 = new PersonDetail();
+            Console.WriteLine("Name: " + pobj1.Name + " Age: " + pobj1.Age);
+            PersonDetail pobj2 = new PersonDetail("Inatech", 30);
+            Console.WriteLine("Name: " + pobj2.Name + " Age: " + pobj2.Age);
+        }
+    }
+}
